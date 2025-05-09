c41e70236fd60e07e90d4f96847349a7ca411cb4
Phexi <phexicolonthree@gmail.com>
2025-05-09 15:47:38 +0200

    Remove need for HMs taught to team

--- src/scrcmd.c
+++ src/scrcmd.c
@@ -2260,7 +2260,7 @@ bool8 ScrCmd_checkpartymove(struct ScriptContext *ctx)
         u16 species = GetMonData(&gPlayerParty[i], MON_DATA_SPECIES, NULL);
         if (!species)
             break;
-        if (!GetMonData(&gPlayerParty[i], MON_DATA_IS_EGG) && MonKnowsMove(&gPlayerParty[i], moveId) == TRUE)
+        if (!GetMonData(&gPlayerParty[i], MON_DATA_IS_EGG))
         {
             gSpecialVar_Result = i;
             gSpecialVar_0x8004 = species;
